c1a6f97fc24321c93995b11efdbb443d29370223
Max Herbold <49804267+Max-Herbold@users.noreply.github.com>
2024-08-21 13:07:22 +1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+from api import api
+from frontend.loader import css_loader, html, js_loader
+
+app = Flask(__name__)
+app.sockets = []
+
+# Register backend
+app.register_blueprint(api)
+
+
+# Register frontend
+app.register_blueprint(css_loader)
+app.register_blueprint(js_loader)
+
+
+@app.route("/")
+@app.route("/<path>.html")
+def hello_world(path=None):
+    """
+    The `hello_world` function returns the HTML content of a specified path or defaults to "index.html".
+
+    Parameters
+    ----------
+    - path: The `path` parameter in the `hello_world` function is a string that represents the file path
+    of the HTML file to be returned. If no `path` is provided when calling the function, it defaults to
+    "index".
+
+    Returns
+    ----------
+    - The function `hello_world` is returning the HTML content of the file specified by the `path`
+    parameter. If `path` is not provided, it defaults to "index.html".
+    """
+    if path is None:
+        path = "index"
+    return html(f"{path}.html")
+
+
+if __name__ == "__main__":
+    app.run(ssl_context=("cert.pem", "key.pem"), port=443)
