50e2e0c1afe7c7e32d2cef698fca0aa039ba80e4
Max Herbold <49804267+Max-Herbold@users.noreply.github.com>
2024-08-28 23:03:01 +1000

    update remote pull and reload

--- api/updater/endpoint.py
+++ api/updater/endpoint.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Blueprint, request
+import requests
+from flask import Blueprint, abort, request
 
 from ..env_loader import load_env
 
@@ -10,7 +11,6 @@ update_bp = Blueprint("update", __name__, url_prefix="/update")
 def _update():
     # load env
     env_vars = load_env()
-    print(env_vars)
 
     token = env_vars.get("GH_TOKEN")
 
@@ -18,22 +18,20 @@ def _update():
     command = (
         f"git pull https://Max-Herbold:{token}@github.com/Max-Herbold/inpac_remote.git"
     )
-    print("running pull", command)
-
-    print(os.getcwd())
 
     # move to dir
     os.chdir("/home/inpac/inpac_remote")
     # git pull https://Max-Herbold:***@github.com/Max-Herbold/inpac_remote.git
-    os.system(command)
-    print("done")
+    return os.system(command)
 
 
 # /api/update/update
 @update_bp.route("/update")
 def update():
-    print("update hit")
     # run "/home/inpac/inpac_remote/updater.py"
     # subprocess.run([sys.executable, "/home/inpac/inpac_remote/updater.py"])
-    _update()
+
+    r = _update()
+    if r != 0:
+        return {"response": "Update failed"}, 500
     return {"response": "Update complete"}, 200
