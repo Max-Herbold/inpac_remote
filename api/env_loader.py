a3f30db1d3fe08fe569dba3aac413a2f9f6bbe7b
Max Herbold <49804267+Max-Herbold@users.noreply.github.com>
2024-08-28 23:03:01 +1000

    update remote pull and reload

--- api/env_loader.py
+++ api/env_loader.py
@@ -1,5 +1,4 @@
 import os
-from typing import Dict
 
 from dotenv import dotenv_values
 
@@ -8,8 +7,5 @@ def load_env() -> dict[str, str | None]:
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
     # go up one directory to find the .env file
     BASEDIR = os.path.dirname(BASEDIR)
-    print("env_loader.py: root", os.path.join(BASEDIR, ".env"))
 
-    res = dotenv_values(os.path.join(BASEDIR, ".env"))
-    print("env_loader.py: loaded env", res)
-    return res
+    return dotenv_values(os.path.join(BASEDIR, ".env"))
