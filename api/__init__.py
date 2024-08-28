34a1e5b9562f93cefd4e5b5e51bd2dc71ad8e26f
Max Herbold <49804267+Max-Herbold@users.noreply.github.com>
2024-08-28 21:42:09 +1000

    register update bp

--- api/__init__.py
+++ api/__init__.py
@@ -2,8 +2,9 @@ from flask import Blueprint
 
 from .creds.request_code import request_code
 from .creds.user import user_endpoint
-from .test import test_bp
 from .device import device_bp
+from .test import test_bp
+from .updater import update_bp
 
 api = Blueprint("api", __name__, url_prefix="/api")
 
@@ -18,3 +19,4 @@ api.register_blueprint(request_code)
 api.register_blueprint(test_bp)
 api.register_blueprint(user_endpoint)
 api.register_blueprint(device_bp)
+api.register_blueprint(update_bp)
