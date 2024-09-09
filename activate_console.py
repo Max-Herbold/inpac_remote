ecc9642ef82ece42ca60937b07f3b99867f5f4a6
Max Herbold <49804267+Max-Herbold@users.noreply.github.com>
2024-09-09 15:56:47 +1000

    staging

--- activate_console.py
+++ activate_console.py
@@ -4,5 +4,3 @@ args = sys.argv[1:]
 chromedriver_path = args[0]
 user = args[1]
 api_key = args[2]
-
-
--- api/device/device.py
+++ api/device/device.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, Flask, request
 
 from ..creds import authenticate_user
+
 # from ..db_interface.interface import Database
 
 device_bp = Blueprint("device", __name__, url_prefix="/device")
